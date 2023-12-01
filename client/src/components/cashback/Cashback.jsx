be939bac4ee3ec2c272f99778a7d1da58bf4e97c
souza.victor <souza.victor@grad.ufsc.br>
2023-12-01 19:00:27 -0300

    add blockchain connection with etherjs

--- client/src/components/cashback/Cashback.jsx
+++ client/src/components/cashback/Cashback.jsx
@@ -1,19 +1,88 @@
 import React, {useState} from 'react';
 import CurrencyExchangeIcon from '@mui/icons-material/CurrencyExchange';
+import { ethers } from 'ethers';
 import "./Cashback.scss"
+import ABI from "../../../abi.json";
 function Cashback(props) {
     const [cupomMessage, setCupomMessage] = useState("");
     const [quantity, setQuantity] = useState(1);
+    const [responseMessage, setResponseMessage] = useState("");
+    const [balanceMessage, setBalanceMessage] = useState("");
+    const [tokenId, setTokenId] = useState("1");
+    const [tokensToBurn, setTokensToBurn] = useState(0);
 
     const images = [
     "https://img.freepik.com/psd-gratuitas/pacote-de-notas-de-dolar-com-icone-de-moeda-isolado-ilustracao-de-renderizacao-3d_47987-7815.jpg?w=826&t=st=1701313307~exp=1701313907~hmac=4b0c1447fd80990256ddfa91bc8ca85cdaa16dc0f065aac89ee4d122289ad159",
     ]
-    function genCashbackCupom() {
-        //TODO burn
 
-        setCupomMessage("Your cashback cupom is: BOOCHIN10")
+    const CONTRACT_ADDRESS = "0x890c5b06C3B257673cfb4C0827492c99dA2A7df5";
+    async function getProvider() {
+        if (!window.ethereum) throw new Error(`No MetaMask found!`);
+
+        const provider = new ethers.BrowserProvider(window.ethereum);
+        //ask permission to talk with the blockchain
+        const accounts = await provider.send("eth_requestAccounts", []);
+        if (!accounts || !accounts.length) throw new Error('Wallet not found/allowed!');
+        return provider;
+    }
+
+    async function doSearch() {
+        try{
+            const provider = await getProvider();
+            const contract = new ethers.Contract(CONTRACT_ADDRESS, ABI, provider);
+            console.log("WALALELELET", provider.send("eth_requestAccounts", []));
+            provider.send("eth_requestAccounts", []).then(res => {
+                contract.balanceOf(res[0], tokenId).then(
+                    responseBalance => {
+
+                        const halfTokens = parseInt(responseBalance)/8
+                        setTokensToBurn(halfTokens)
+                        if (halfTokens === 0 ) {
+                            throw new Error("You have 0 tokens.")
+                        }
+                    }
+                );
+            })
+            console.log("oiiii")
+        }catch (e) {
+            console.log(e, e)
+            setCupomMessage("You have 0 tokens. Buy clothes or tokens from another user and comeback.")
+
+    }}
+
+    async function getContractSigner() {
+        const provider = await getProvider();
+        const signer = await provider.getSigner();
+        const contract = new ethers.Contract(CONTRACT_ADDRESS, ABI, provider);
+        return contract.connect(signer);
+    }
+
+    async function doBurn() {
+        try {
+            console.log("to no do save")
+
+            if (tokensToBurn === 0 ) {
+                throw new Error("You have 0 tokens.")
+            }
+            const contract = await getContractSigner();
+            //alert(JSON.stringify(tx));
+            const burn = await contract.burn(parseInt(tokenId), Math.round(tokensToBurn)).then(setCupomMessage("Your cashback cupom is: BOOCHIN10"));
+
+            setResponseMessage(burn)
+            setBalanceMessage("Your balance now: " + (tokensToBurn*7))
+        } catch (e) {
+            setCupomMessage("You have 0 tokens.")
+            console.log("OIOIIIII", e)
+        }
+
+    }
+    function  genCashbackCupom() {
+        let teste = doSearch()
+        doBurn()
+        console.log("tokensBurned", tokensToBurn)
     }
     return (
+
     <div className='cashback'>
         <div className="left">
             <div className="mainImg">
@@ -28,6 +97,7 @@ function Cashback(props) {
                 <CurrencyExchangeIcon/> GET CUPOM
             </button>
             <p className={'cupom'}>{cupomMessage}</p>
+            <p className={'cupom'}>{balanceMessage}</p>
         </div>
     </div>
 
--- client/src/pages/product/Product.jsx
+++ client/src/pages/product/Product.jsx
@@ -4,6 +4,8 @@ import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
 import BalanceIcon from '@mui/icons-material/Balance';
 import { useParams } from 'react-router-dom';
+import {ethers} from "ethers";
+import ABI from "../../../abi.json";
 
 
 
@@ -13,29 +15,113 @@ const Product = () => {
   const [quantity, setQuantity] = useState(1);
   const [tokenBurned, setTokenBurned] = useState(false);
 
+  const [cupomMessage, setCupomMessage] = useState("");
+  const [responseMessage, setResponseMessage] = useState("");
+  const [balanceMessage, setBalanceMessage] = useState("");
+  const [tokenId, setTokenId] = useState("2");
+  const [tokensToBurn, setTokensToBurn] = useState(1);
+
+
   const images = [
     "https://images.pexels.com/photos/5886042/pexels-photo-5886042.jpeg?auto=compress&cs=tinysrgb&w=1600",
     "https://images.pexels.com/photos/5119526/pexels-photo-5119526.jpeg?auto=compress&cs=tinysrgb&w=1600"
   ]
+  const CONTRACT_ADDRESS = "0x890c5b06C3B257673cfb4C0827492c99dA2A7df5";
+  async function getProvider() {
+    if (!window.ethereum) throw new Error(`No MetaMask found!`);
 
+    const provider = new ethers.BrowserProvider(window.ethereum);
+    //ask permission to talk with the blockchain
+    const accounts = await provider.send("eth_requestAccounts", []);
+    if (!accounts || !accounts.length) throw new Error('Wallet not found/allowed!');
+    return provider;
+  }
+
+  async function doSearch() {
+    try{
+      const provider = await getProvider();
+      const contract = new ethers.Contract(CONTRACT_ADDRESS, ABI, provider);
+      console.log("WALALELELET", provider.send("eth_requestAccounts", []));
+      provider.send("eth_requestAccounts", []).then(res => {
+        contract.balanceOf(res[0], tokenId).then(
+            responseBalance => {
+
+              const halfTokens = parseInt(responseBalance)/8
+              setTokensToBurn(halfTokens)
+              if (halfTokens === 0 ) {
+                throw new Error("You have 0 tokens.")
+              }
+            }
+        );
+      })
+      console.log("oiiii")
+    }catch (e) {
+      console.log(e, e)
+      setCupomMessage("You have 0 tokens. Buy clothes or tokens from another user and comeback.")
+
+    }}
+
+  async function getContractSigner() {
+    const provider = await getProvider();
+    const signer = await provider.getSigner();
+    const contract = new ethers.Contract(CONTRACT_ADDRESS, ABI, provider);
+    return contract.connect(signer);
+  }
+
+  async function doBurn() {
+    try {
+      console.log("to no do save")
+
+      if (tokensToBurn === 0 ) {
+        throw new Error("You have 0 tokens.")
+      }
+      const contract = await getContractSigner();
+      //alert(JSON.stringify(tx));
+      const burn = await contract.burn(parseInt(tokenId), Math.round(tokensToBurn)).then(setTokenBurned(true));
+
+      setResponseMessage(burn)
+      setBalanceMessage("Your balance now: " + (tokensToBurn*7))
+    } catch (e) {
+      setCupomMessage("You have 0 tokens.")
+      console.log("OIOIIIII", e)
+    }}
+  async function doMint() {
+    try {
+      const provider = await getProvider();
+      const contract = await getContractSigner();
+      //alert(JSON.stringify(tx));
+      let numToken = quantity * 130
+      provider.send("eth_requestAccounts", []).then(res => { contract.mint_with_barcode(res[0], 1234567891, numToken).then(setResponseMessage("You just recceived 1 token for each clothe buyed."))})
+      console.log("numtoken", numToken)
+
+    } catch (e) {
+      setCupomMessage("Some error ocurred. Contact the chat support")
+      console.log("error do mint", e)
+    }}
   function burnToken() {
-    //TODO implementar burn token
-    setTokenBurned(true);
+    let teste = doSearch()
+    doBurn()
+    console.log("tokensBurned", tokensToBurn)
+    //setTokenBurned(true)
   }
 
   function transferToken() {
     //TODO implementar função que transfer token quando comprando (mint_with_barcode)
     //exibir mensagem ou alerta, algo do tipo, informando
     //usar barcode hardcoded
+    doMint()
   }
 
   const makeBuyButton = () => {
     if(catId === 2) {
       //case tokens
       return (
-        <button className='addButton' onClick={() => transferToken()}>
-          <AddShoppingCartIcon/> BUY NOW
-        </button>
+          <div>
+            <button className='addButton' onClick={() => transferToken()}>
+              <AddShoppingCartIcon/> BUY NOW
+            </button>
+            <p>{responseMessage}</p>
+          </div>
       )
     }
     //case exclusive collecion
